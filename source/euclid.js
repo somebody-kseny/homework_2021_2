548c7d9af9ced0a5a4fe37c5cd8fdc9b511aade0
Somebody_Kseny <skk1020@yandex.ru>
2021-09-02 13:43:11 +0300

    fixes #3. things about code style

--- source/euclid.js
+++ source/euclid.js
@@ -1,4 +1,23 @@
-'use strict'; // Это директива, которая заставляет использовать новый (5-й), несовместимый с предыдущими стандарт JavaScript
+'use strict'; // Это директива, которая заставляет использовать новый (5-й),
+              // несовместимый с предыдущими стандарт JavaScript
+
+/**
+  * Функция находит наибольший общий делитель произвольного набора натуральных чисел
+  * 
+  * @param {...number} numbers - числа, для которых нужно найти НОД
+  * @returns {number} - НОД
+  */
+
+ const euclid = (...numbers) => {
+    if (numbers.length < 1){
+        return undefined;
+    }
+    let currentNod = numbers[0];
+    numbers.forEach((item) => {
+        currentNod = findNOD(currentNod, item);
+    });
+    return currentNod;
+};
 
 /**
   * Функция находит наибольший общий делитель двух чисел, используя алгоритм Евклида
@@ -8,35 +27,19 @@
   * @returns {number} - НОД
   */
 
-const findNOD = (left, right) => {
+ const findNOD = (left, right) => {
     if ( (isNaN(left)) || (isNaN(right)) || (left === null) || (right === null)) {
-        return undefined
+    //if ( ( isNaN(left) ) || ( isNaN(right) ) ) {
+        return undefined;
     }
 
     while ( (left != 0) && (right != 0) ) {
         if (left > right){
-            left = left % right
+            left = left % right;
         } else {
-            right = right % left
+            right = right % left;
         }
-    }
-    return (left + right)
-}
+    };
 
-/**
-  * Функция находит наибольший общий делитель произвольного набора натуральных чисел
-  * 
-  * @param {...number} numbers - числа, для которых нужно найти НОД
-  * @returns {number} - НОД
-  */
-
-const euclid = (...numbers) => {
-    if (numbers.length < 1){
-        return undefined
-    }
-    let current_nod = numbers[0];
-    numbers.forEach((item) => {
-        current_nod = findNOD(current_nod, item);
-    });
-    return current_nod;
-}
+    return (left + right);
+};
