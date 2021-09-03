7df10ea253dea50fba6fd052f11a829956fe9ff2
Somebody_Kseny <skk1020@yandex.ru>
2021-09-03 22:04:19 +0300

    no unnecessary () at all ; 4 spaces -> 2 spaces; eg

--- source/euclid.js
+++ source/euclid.js
@@ -1,51 +1,52 @@
 // Это директива, которая заставляет использовать новый (5-й),
 // несовместимый с предыдущими стандарт JavaScript
-'use strict'; 
+'use strict';
 
 /**
-  * Функция находит наибольший общий делитель (НОД) произвольного набора натуральных чисел
-  * 
-  * @param {...number} numbers - числа, для которых нужно найти НОД
-  * @returns { (number|undefined) } - число, НОД, если на вход поданы натуральные числа. 
-  *     Иначе - undefined
-  */
+ * Функция находит наибольший общий делитель (НОД) произвольного набора натуральных чисел
+ * 
+ * @param {...number} numbers - числа, для которых нужно найти НОД
+ * @returns { number|undefined } - число, НОД, если на вход поданы натуральные числа. Иначе - undefined
+ */
 
- const euclid = (...numbers) => {
-    if (numbers.length < 1){
-        return undefined;
-    }
-    let currentNod = numbers[0];
-    numbers.forEach((item) => {
-        currentNod = findNOD(currentNod, item);
-    });
-    return currentNod;
+const euclid = (...numbers) => {
+  if (numbers.length < 1) {
+    return undefined;
+  }
+
+  let currentNod = numbers[0];
+  numbers.forEach(item => {
+    currentNod = findNOD(currentNod, item);
+  });
+
+  return currentNod;
 };
 
 /**
-  * Функция находит наибольший общий делитель (НОД) двух чисел, используя алгоритм Евклида
-  * 
-  * @param {number} left - первое число
-  * @param {number} right - второе число
-  * @returns { (number|undefined) } - число, НОД, если на вход поданы два натуральных числа. 
-  *     Иначе - undefined
-  */
+ * Функция находит наибольший общий делитель (НОД) двух чисел, используя алгоритм Евклида
+ *
+ * @param {number} left - первое число
+ * @param {number} right - второе число
+ * @returns { number|undefined} - число, НОД, если на вход поданы два натуральных числа. 
+ *     Иначе - undefined
+ */
 
 const findNOD = (left, right) => {
-    if (  typeof left !== "number"  ||  typeof right !== "number"  ) {
-        return undefined;
+  if (typeof left !== 'number' || typeof right !== 'number') {
+    return undefined;
+  }
+
+  if (left <= 0 || right <= 0) {
+    return undefined;
+  }
+
+  while (left != 0 && right != 0) {
+    if (left > right) {
+      left = left % right;
+    } else {
+      right = right % left;
     }
+  };
 
-    if ( left <= 0 || right <= 0 ) {
-        return undefined;
-    }
-
-    while ( (left != 0) && (right != 0) ) {
-        if (left > right){
-            left = left % right;
-        } else {
-            right = right % left;
-        }
-    };
-
-    return (left + right);
+  return (left + right);
 };
